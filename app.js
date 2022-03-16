5aa0d8faad6dfd3b76b034bec81a983cad984a69
bharatbhushanrai <70462142+Bharat-Bhushan-Rai@users.noreply.github.com>
2022-03-16 16:34:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const res = require("express/lib/response");
+// const res = require("express/lib/response");
 
 const app=express();
 app.set('view engine', 'ejs');
